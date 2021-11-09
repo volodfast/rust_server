5d117fda625ccfe438a6f166dc95cad5544a3d1c
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-10 00:21:31 +0200

    Design server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Server is running on: {}!", self.addr);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = String::from("127.0.0.1:8080");
+
+    let server = Server::new(addr);
+    server.run();
 }
